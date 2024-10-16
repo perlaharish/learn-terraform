db02807f17cb823b947ca1ddbad9550d986fd6d1
perlaharish <perlaharish458@gmail.com>
2024-10-16 20:08:01 +0530

    sample terraform script to create ec2 server

--- Sample/helloworld.tf
+++ Sample/helloworld.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-0634ac0aab4eb2831"
   instance_type = "t3.micro"
-  tags = {
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+tags = {
     Name = "HelloWorld"
   }
 }
