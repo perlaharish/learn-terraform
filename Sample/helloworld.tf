927adadb13bf6bee9baaa179157e43aacf744001
perlaharish <perlaharish458@gmail.com>
2024-10-16 19:50:28 +0530

    sample terraform script to create ec2 server

--- Sample/helloworld.tf
+++ Sample/helloworld.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = ami-0634ac0aab4eb2831
+  ami           = "ami-0634ac0aab4eb2831"
   instance_type = "t3.micro"
   tags = {
     Name = "HelloWorld"
