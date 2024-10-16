fc8eabe47219897a11a2802967eef9ccf036ce75
perlaharish <perlaharish458@gmail.com>
2024-10-16 20:57:26 +0530

    Added all services ec2 scripts

--- ecom-v1/common.tf
+++ ecom-v1/common.tf
@@ -13,3 +13,83 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [ aws_instance.frontend.private_ip ]
 }
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-0634ac0aab4eb2831"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-020b89fdba5e82e18"]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
